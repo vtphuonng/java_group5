17805bd977c0d5d6d5a12d34e8ba0e2c24de27a7
= <=>
2023-05-06 16:48:53 +0700

    connect with database

--- src/main/java/com/example/demo/Product/Product.java
+++ src/main/java/com/example/demo/Product/Product.java
@@ -1,16 +1,22 @@
 package com.example.demo.Product;
 
+
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="Homestay")
 public class Product {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
+	@Column(nullable = false,unique = true, length=300)
 	private String name;
 	private String address;
 	private String contact;
--- src/main/java/com/example/demo/database/database.java
+++ src/main/java/com/example/demo/database/database.java
@@ -10,6 +10,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 
+/*
+docker run -d --rm --name mysql-spring-boot-tutorial -e MYSQL_ROOT_PASSWORD=Cu@moni@c8@5 -e MYSQL_USER=test -e MYSQL_PASSWORD=Cu@moni@c8@5 -e MYSQL_DATABASE=test_db -p 3309:3306 --volume mysql-spring-boot-tutorial-volume:/var/lib/mysql mysql:latestmysql -h localhost -P 3309 --protocol=tcp -u test -p
+
+* */
+
 @Configuration
 public class database {
 	
@@ -19,10 +24,11 @@ public class database {
 		return new CommandLineRunner() {
 			@Override
 			public void run(String...args) throws Exception{
-				Product productA = new Product(1,"sdfs","asdasd","asdfaf","asdfa", 121L, 2342L);
+/*				Product productA = new Product(1,"sdfs","asdasd","asdfaf","asdfa", 121L, 2342L);
 				Product productB = new Product(2, "hois","hoh","bo","gi", 1771L, 2882L);
 				logger.info("insert data: "+repository.save(productA));
 				logger.info("insert data: "+repository.save(productB));
+*/
 			}
 		};
 	}
